30f9fa0cf5d8584756e43b0646e79d324c861d30
NgocVii <vi2872005@gmail.com>
2025-04-20 16:51:55 +0700

    Add files via upload

---
+++ Lab01/Lab01_10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int n;
+Console.WriteLine("Nhap so nguyen: ");
+n = int.Parse(Console.ReadLine());
+int dem = 0;
+for (int i = 1; i <= n; i++)
+{
+    if (n % i == 0)
+    {
+        dem++;
+    }
+}
+if (dem == 2)
+{
+    Console.WriteLine($"{n} la so nguyen to");
+}
+else
+{
+    Console.WriteLine($"{n} khong la so nguyen to");
+}

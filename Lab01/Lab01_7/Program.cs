5e19679606daa22eb51b6e907b82def37f418272
NgocVii <vi2872005@gmail.com>
2025-04-20 16:50:42 +0700

    Add files via upload

---
+++ Lab01/Lab01_7/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+int nam;
+Console.WriteLine("Nhap nam: ");
+nam = int.Parse(Console.ReadLine());
+if (nam % 100 == 0)
+{
+    if (nam % 4 == 0)
+    {
+        Console.WriteLine($"{nam} la nam nhuan");
+    }
+    else
+    {
+        Console.WriteLine($"{nam} khong la nam nhuan");
+    }
+}
+else
+{
+    if (nam % 4 == 0)
+    {
+        Console.WriteLine($"{nam} la nam nhuan");
+    }
+    else
+    {
+        Console.WriteLine($"{nam} khong la nam nhuan");
+    }
+}
---
+++ Lab01/Lab01_8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+for (int i = 1; i <= 10; i++)
+{
+    Console.WriteLine($"Bang nhan voi so {i}");
+    for (int j = 1; j <= 10; j++)
+    {
+        Console.WriteLine($"{i}x{j}={i * j}");
+    }
+}
